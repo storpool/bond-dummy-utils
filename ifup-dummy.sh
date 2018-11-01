ba85a02e9e195ff21eee5ab1cc1a29d357561f7f
Peter Pentchev <pp@storpool.com>
2018-11-01 10:19:47 +0200

    Add a copyright notice and a GPL-2 license blurb.
    The license is the same as the one for the network-scripts/ tools in
    the fedora-sysv/initscripts package.

--- ifup-dummy.sh
+++ ifup-dummy.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+# Copyright (c) 2018  StorPool
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# This tool is inspired by similar tools in the fedora-sysv/initscripts
+# package which carry the following copyright notice:
+#
+# Copyright (c) 1996-2014 Red Hat, Inc. all rights reserved.
 
 . /etc/init.d/functions
 
